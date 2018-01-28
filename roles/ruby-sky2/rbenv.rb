dd24b762e318bbb247284e806c16b6b5c4fff0d2
Takashi Kokubun <takashikkbn@gmail.com>
2018-01-28 23:06:06 +0900

    Install rbenv-each

--- roles/ruby-sky2/rbenv.rb
+++ roles/ruby-sky2/rbenv.rb
@@ -5,3 +5,7 @@ end
 execute 'git clone https://github.com/rbenv/ruby-build /home/k0kubun/.rbenv/plugins/ruby-build' do
   not_if 'test -d /home/k0kubun/.rbenv/plugins/ruby-build'
 end
+
+execute 'git clone https://github.com/rbenv/rbenv-each /home/k0kubun/.rbenv/plugins/rbenv-each' do
+  not_if 'test -d /home/k0kubun/.rbenv/plugins/rbenv-each'
+end
