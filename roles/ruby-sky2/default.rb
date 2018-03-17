959eeb9e3d2996d2dbdcebc17c382114ec2fd599
Takashi Kokubun <takashikkbn@gmail.com>
2018-03-17 16:05:30 +0900

    Install skybench timer

--- roles/ruby-sky2/default.rb
+++ roles/ruby-sky2/default.rb
@@ -1,4 +1,5 @@
 # You need to put deploy key for github.com:benchmark-driver/skybench on ruby-sky2.
+# Updating known_hosts is not automated as well.
 
 include_recipe 'rbenv'
 include_recipe 'ssh'
@@ -25,4 +26,33 @@ end
 
 execute 'git clone --recursive git@github.com:benchmark-driver/skybench /home/k0kubun/skybench' do
   not_if 'test -d /home/k0kubun/skybench'
+end
+
+%w[
+  /home/k0kubun/.config
+  /home/k0kubun/.config/systemd
+  /home/k0kubun/.config/systemd/user
+  /home/k0kubun/.config/systemd/user/timers.target.wants
+].each do |dir|
+  directory dir do
+    mode '755'
+    owner 'k0kubun'
+    group 'k0kubun'
+  end
+end
+
+remote_file '/home/k0kubun/.config/systemd/user/skybench.service' do
+  mode '600'
+  owner 'k0kubun'
+  group 'k0kubun'
+end
+
+remote_file '/home/k0kubun/.config/systemd/user/skybench.timer' do
+  mode '600'
+  owner 'k0kubun'
+  group 'k0kubun'
+end
+
+link '/home/k0kubun/.config/systemd/user/timers.target.wants/skybench.timer' do
+  to '/home/k0kubun/.config/systemd/user/skybench.timer'
 end
