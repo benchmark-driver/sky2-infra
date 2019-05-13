92e914d7b67d7d2b635f053a680b85d3643365bd
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-13 21:56:52 +0900

    Build old ruby and new ruby

--- roles/ruby-sky2/default.rb
+++ roles/ruby-sky2/default.rb
@@ -4,14 +4,36 @@
 include_recipe 'rbenv'
 include_recipe 'ssh'
 
-%w[
-  2.0.0-p648
-  2.1.10
-  2.2.10
-  2.3.8
-  2.4.6
-  2.5.5
-  2.6.3
+openssl_version = '1.0.2r'
+execute "install openssl-#{openssl_version}" do
+  command <<-EOS
+    cd /tmp
+    wget https://www.openssl.org/source/openssl-#{openssl_version}.tar.gz
+    tar xvzf openssl-#{openssl_version}.tar.gz
+    cd openssl-#{openssl_version}
+    CFLAGS=-fPIC ./config shared --prefix=/home/k0kubun/openssl-#{openssl_version}
+    make -j4
+    make install
+  EOS
+  not_if "test -d /home/k0kubun/openssl-#{openssl_version}"
+end
+
+[
+  '2.0.0-p648',
+  '2.1.10',
+  '2.2.10',
+  '2.3.8',
+].each do |version|
+  configure_opts = "--with-openssl-dir=/home/k0kubun/openssl-#{openssl_version}"
+  execute "env RUBY_CONFIGURE_OPTS='#{configure_opts}' /home/k0kubun/.rbenv/bin/rbenv install #{version}" do
+    not_if "test -d /home/k0kubun/.rbenv/versions/#{version}"
+  end
+end
+
+[
+  '2.4.6',
+  '2.5.5',
+  '2.6.3',
 ].each do |version|
   execute "/home/k0kubun/.rbenv/bin/rbenv install #{version}" do
     not_if "test -d /home/k0kubun/.rbenv/versions/#{version}"
