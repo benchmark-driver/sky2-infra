697ca85d94d659d1ab8bed6a562c07ed1a6ec021
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-13 21:08:33 +0900

    Comment out majority of code for now

--- roles/ruby-sky2/default.rb
+++ roles/ruby-sky2/default.rb
@@ -4,61 +4,61 @@
 include_recipe 'rbenv'
 include_recipe 'ssh'
 
-%w[
-  2.0.0-p648
-  2.1.10
-  2.2.9
-  2.3.6
-  2.4.3
-  2.5.0
-  2.6.0-preview1
-].each do |version|
-  execute "/home/k0kubun/.rbenv/bin/rbenv install #{version}" do
-    not_if "test -d /home/k0kubun/.rbenv/versions/#{version}"
-  end
-end
+# %w[
+#   2.0.0-p648
+#   2.1.10
+#   2.2.9
+#   2.3.6
+#   2.4.3
+#   2.5.0
+#   2.6.0-preview1
+# ].each do |version|
+#   execute "/home/k0kubun/.rbenv/bin/rbenv install #{version}" do
+#     not_if "test -d /home/k0kubun/.rbenv/versions/#{version}"
+#   end
+# end
 
-remote_file '/home/k0kubun/.bashrc' do
-  mode '644'
-  owner 'k0kubun'
-  group 'k0kubun'
-end
+# remote_file '/home/k0kubun/.bashrc' do
+#   mode '644'
+#   owner 'k0kubun'
+#   group 'k0kubun'
+# end
 
-remote_file '/home/k0kubun/.gitconfig' do
-  mode '644'
-  owner 'k0kubun'
-  group 'k0kubun'
-end
+# remote_file '/home/k0kubun/.gitconfig' do
+#   mode '644'
+#   owner 'k0kubun'
+#   group 'k0kubun'
+# end
 
-execute 'git clone --recursive git@github.com:benchmark-driver/skybench /home/k0kubun/skybench' do
-  not_if 'test -d /home/k0kubun/skybench'
-end
+# execute 'git clone --recursive git@github.com:benchmark-driver/skybench /home/k0kubun/skybench' do
+#   not_if 'test -d /home/k0kubun/skybench'
+# end
 
-%w[
-  /home/k0kubun/.config
-  /home/k0kubun/.config/systemd
-  /home/k0kubun/.config/systemd/user
-  /home/k0kubun/.config/systemd/user/timers.target.wants
-].each do |dir|
-  directory dir do
-    mode '755'
-    owner 'k0kubun'
-    group 'k0kubun'
-  end
-end
+# %w[
+#   /home/k0kubun/.config
+#   /home/k0kubun/.config/systemd
+#   /home/k0kubun/.config/systemd/user
+#   /home/k0kubun/.config/systemd/user/timers.target.wants
+# ].each do |dir|
+#   directory dir do
+#     mode '755'
+#     owner 'k0kubun'
+#     group 'k0kubun'
+#   end
+# end
 
-remote_file '/home/k0kubun/.config/systemd/user/skybench.service' do
-  mode '600'
-  owner 'k0kubun'
-  group 'k0kubun'
-end
+# remote_file '/home/k0kubun/.config/systemd/user/skybench.service' do
+#   mode '600'
+#   owner 'k0kubun'
+#   group 'k0kubun'
+# end
 
-remote_file '/home/k0kubun/.config/systemd/user/skybench.timer' do
-  mode '600'
-  owner 'k0kubun'
-  group 'k0kubun'
-end
+# remote_file '/home/k0kubun/.config/systemd/user/skybench.timer' do
+#   mode '600'
+#   owner 'k0kubun'
+#   group 'k0kubun'
+# end
 
-link '/home/k0kubun/.config/systemd/user/timers.target.wants/skybench.timer' do
-  to '/home/k0kubun/.config/systemd/user/skybench.timer'
-end
+# link '/home/k0kubun/.config/systemd/user/timers.target.wants/skybench.timer' do
+#   to '/home/k0kubun/.config/systemd/user/skybench.timer'
+# end
